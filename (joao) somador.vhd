3c79cc5dd14499946718cfd56b5e3c2bc006b0a4
Nicolas Evangelista <nicolasabreu87@gmail.com>
2025-10-19 13:50:57 -0300

    Adder Ready

--- (joao) somador.vhd
+++ (joao) somador.vhd
@@ -4,13 +4,13 @@ use ieee.numeric_std.all;
 
 entity somador is
     port(
-        somador_in  : in  std_logic_vector(15 downto 0);
-        somador_out : out std_logic_vector(15 downto 0)
+        somador_in  : in  std_logic_vector(31 downto 0);
+        somador_out : out std_logic_vector(31 downto 0)
     );
 end entity;
 
 architecture rtl of somador is
 begin
-    -- somador combinacional
-    somador_out <= std_logic_vector(unsigned(somador_in) + 1);
+    -- somador combinacional: PC + 4 (tava antes PC + 1)
+    somador_out <= std_logic_vector(unsigned(somador_in) + 4);
 end architecture;
